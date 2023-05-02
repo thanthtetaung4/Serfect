aa863b547cfbd2973680f990048c02f3ae91f9aa
Thant Htet Aung <maung.good@gmail.com>
2023-05-02 14:32:44 +0800

    About Fix
    Fixed merge confilts and added articles to the tour page.

--- scripts/tour-script.js
+++ scripts/tour-script.js
@@ -1,8 +1,38 @@
-const filterSeeMoreBtn = document.querySelector(".filter-see-btn-container");
-console.log(filterSeeMoreBtn);
-const filterArea = document.querySelector(".filter-container");
-let isFilterAreaOpen = false;
-filterSeeMoreBtn.addEventListener("click", toggleFilterArea);
+// const filterSeeMoreBtn = document.querySelector(".filter-see-btn-container");
+// console.log(filterSeeMoreBtn);
+// const filterArea = document.querySelector(".filter-container");
+// let isFilterAreaOpen = false;
+// filterSeeMoreBtn.addEventListener("click", toggleFilterArea);
+const pdfButton = document.querySelector("#pdf-button");
+console.log(pdfButton);
+pdfButton.addEventListener("click", printToPdf);
+
+function printToPdf() {
+  // create a new jsPDF instance
+  const doc = new jsPDF({
+    orientation: "portrait",
+    unit: "mm",
+    format: "a4",
+  });
+
+  // get the HTML content to print
+  const content = document.querySelector(".print-area");
+  console.log(content);
+  content.classList.add("to-print");
+  // render the HTML content to a canvas
+  html2canvas(content, {
+    allowTaint: true,
+    useCORS: true,
+  }).then((canvas) => {
+    // add the canvas as an image to the PDF document
+    const imgData = canvas.toDataURL("image/png");
+    doc.addImage(imgData, "PNG", 0, 0, 210, 297);
+
+    // save the PDF file and open it in a new tab
+    doc.save("your-plan.pdf");
+  });
+  content.classList.remove("to-print");
+}
 
 function toggleFilterArea() {
   // console.log("clicked");
