cc6ac911dd117b8f8c5833ce13e36aa6f448eb19
Thant Htet Aung <maung.good@gmail.com>
2023-05-02 09:53:52 +0800

    error fixing and article and how to templates design update
    Fixed errors in about page and make the design consistance for article and how to templates

--- scripts/about-script.js
+++ scripts/about-script.js
@@ -12,16 +12,16 @@ avatarOne.addEventListener("click", function () {
   avatarFour.classList.remove("bot-open");
   avatarFive.classList.remove("bot-open");
   const name = avatarOne.querySelector("h3").innerHTML;
-  // console.log(name[name.length - 1]);
-  const textID = "#person-" + name[name.length - 1];
-  // console.log(textID);
+  console.log(name[name.length - 1]);
+  const textID = "#person-1";
+  console.log(textID);
   const details = document.querySelector(textID);
-  // console.log(details);
+  console.log(details);
   details.classList.add("show");
   for (let i = 1; i < 6; i++) {
-    if (i != name[name.length - 1]) {
+    if (i != 1) {
       const textID = "#person-" + i;
-      // console.log(textID);
+      console.log(textID);
       let details = document.querySelector(textID);
       details.classList.remove("show");
     }
@@ -33,18 +33,18 @@ avatarTwo.addEventListener("click", function () {
   avatarThree.classList.remove("bot-open");
   avatarFour.classList.remove("bot-open");
   avatarFive.classList.remove("bot-open");
-  // console.log("Avatar 2 clicked");
+  console.log("Avatar 2 clicked");
   const name = avatarTwo.querySelector("h3").innerHTML;
-  // console.log(name[name.length - 1]);
-  const textID = "#person-" + name[name.length - 1];
-  // console.log(textID);
+  console.log(name[name.length - 1]);
+  const textID = "#person-2";
+  console.log(textID);
   const details = document.querySelector(textID);
-  // console.log(details);
+  console.log(details);
   details.classList.add("show");
   for (let i = 1; i < 6; i++) {
-    if (i != name[name.length - 1]) {
+    if (i != 2) {
       const textID = "#person-" + i;
-      // console.log(textID);
+      console.log(textID);
       let details = document.querySelector(textID);
       details.classList.remove("show");
     }
@@ -59,13 +59,13 @@ avatarThree.addEventListener("click", function () {
   // console.log("Avatar 3 clicked");
   const name = avatarThree.querySelector("h3").innerHTML;
   // console.log(name[name.length - 1]);
-  const textID = "#person-" + name[name.length - 1];
+  const textID = "#person-3";
   // console.log(textID);
   const details = document.querySelector(textID);
   // console.log(details);
   details.classList.add("show");
   for (let i = 1; i < 6; i++) {
-    if (i != name[name.length - 1]) {
+    if (i != 3) {
       const textID = "#person-" + i;
       // console.log(textID);
       let details = document.querySelector(textID);
@@ -82,13 +82,13 @@ avatarFour.addEventListener("click", function () {
   // console.log("Avatar 4 clicked");
   const name = avatarFour.querySelector("h3").innerHTML;
   // console.log(name[name.length - 1]);
-  const textID = "#person-" + name[name.length - 1];
+  const textID = "#person-4";
   // console.log(textID);
   const details = document.querySelector(textID);
   // console.log(details);
   details.classList.add("show");
   for (let i = 1; i < 6; i++) {
-    if (i != name[name.length - 1]) {
+    if (i != 4) {
       const textID = "#person-" + i;
       // console.log(textID);
       let details = document.querySelector(textID);
@@ -103,11 +103,11 @@ avatarFive.addEventListener("click", function () {
   avatarFour.classList.remove("bot-open");
   avatarFive.classList.add("bot-open");
   const name = avatarFive.querySelector("h3").innerHTML;
-  const textID = "#person-" + name[name.length - 1];
+  const textID = "#person-5";
   const details = document.querySelector(textID);
   details.classList.add("show");
   for (let i = 1; i < 6; i++) {
-    if (i != name[name.length - 1]) {
+    if (i != 5) {
       const textID = "#person-" + i;
       // console.log(textID);
       let details = document.querySelector(textID);
